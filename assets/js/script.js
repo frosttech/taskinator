4bfce19c2e5e7d8529cf0227f301d0ef40b148b8
frosttech <nick@frosttech.dev>
2022-05-14 14:41:54 -0500

    add task type to list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
 
 // assign objects to variables
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEL = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 // callback function for creating new list element
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     // creates and formats list object
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    // appends new list object to task list
-    tasksToDoEL.appendChild(listItemEl);
+
+    // creates a div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML contents to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl); 
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
 }
 
-
 // add task when button is clicked
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
