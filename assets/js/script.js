3e91b5f542e7f402720efd9c386079bfba8ad3cf
frosttech <nick@frosttech.dev>
2022-05-14 13:47:40 -0500

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 
+// assign objects to variables
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEL = document.querySelector("#tasks-to-do");
+
+
+// callback function for creating new list element
+var createTaskHandler = function() {
+    // creates and formats list object
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    // appends new list object to task list
+    tasksToDoEL.appendChild(listItemEl);
+}
+
+
+// add task when button is clicked
+buttonEl.addEventListener("click", createTaskHandler);
